522f7162e03ad32d53a21a85b71c81fc770c5f59
arkngl <adanartola272@gmail.com>
2018-10-30 15:33:49 -0600

    sixth commit

--- regions.py
+++ regions.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
 
+import pyregion 
 import numpy as np
-import astropy.io.fits as fits 
-
-import pyregion as filter
+import astropy.io.fits as fits
+import pyregion._region_filter as filter
 
 modelname="../data/oa30_i0-zip/t5_p0_q0_oa30_R10_Mcl0.97_i0_total.fits"
 hdulist =fits.open(modelname)
@@ -12,5 +14,29 @@ nx = header ["NAXIS1"]
 ny = header ["NAXIS2"]
 nl = header ["NAXIS3"]
 
+source =pyregion.open("regions.reg")
+nreg = len(source)
+size = (ny, nx)
+tot_sed = np.zeros((nl,nreg))
 
+for k in range(nreg):
+    print "Region ",k
+    source = pyregion.open("regions.reg")
+    del source[0:k]
+    del source[k+1:nreg]
+    mask = source.get_mask(shape=size)
+    sflx = 0.0
+    for m in range(nl):
+        for i in range(nx):
+            for n in range(ny):
+                if mask[n,i] == True:
+                    tot_sed[m,k] = tot_sed[m,k] + cube[m,n,i]
 
+"""
+fmt = "%12.4e %12.4e %12.4e %12.4e %12.4e \n"
+ff = open("sed.dat",'w')
+ff.write("reg1 reg2 reg3 reg4 tot \n")
+for v in zip(sed1, sed2, sed3, sed4, sedt):
+    ff.write(fmt%v)
+ff.close()
+"""
